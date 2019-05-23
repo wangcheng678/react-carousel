b3cfcac5fc2d1e13befcf9bf9832ae3875fc7bb0
Cheng Wang <wangcheng678@gmail.com>
2019-05-24 03:10:41 +0800

    feat: add demo for adding items on the fly

--- example/index.jsx
+++ example/index.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import {render} from 'react-dom'
 import Carousel from '../src/index'
 
-const app = (
-  <Carousel className="container" step={200}>
-    <div>Slide 1</div>
-    <div>Slide 2</div>
-    <div>Slide 3</div>
-    <div>Slide 4</div>
-    <div>Slide 5</div>
-  </Carousel>
-)
+const App = () => {
+  const [added, setAdded] = React.useState(false)
+  return (
+    <React.Fragment>
+      <button type="button" onClick={() => setAdded(a => !a)}>
+        add item
+      </button>
+      <Carousel className="container" step={200}>
+        <div>Slide 1</div>
+        <div>Slide 2</div>
+        <div>Slide 3</div>
+        {added && <div>Slide 4</div>}
+        <div>Slide 5</div>
+      </Carousel>
+    </React.Fragment>
+  )
+}
 
-render(app, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
